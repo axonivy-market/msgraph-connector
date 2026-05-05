55ec47fef40229bf423e3686efc70ea2b3872abd
thxhuy <xuanhuy4444@gmail.com>
2026-05-05 18:28:26 +0700

    Fix test

--- msgraph-connector-test/src_test/com/axonivy/connector/office365/test/GraphTestClient.java
+++ msgraph-connector-test/src_test/com/axonivy/connector/office365/test/GraphTestClient.java
@@ -13,45 +13,24 @@ import ch.ivyteam.ivy.rest.client.security.CsrfHeaderFeature;
 
 public class GraphTestClient {
 
-  public static final UUID GRAPH_CLIENT_ID = UUID.fromString("007036dc-72d1-429f-88a7-ba5d5cf5ae58");
-
-  
+  public static final String GRAPH_CLIENT_ID = "007036dc-72d1-429f-88a7-ba5d5cf5ae58";
   public static void configureFixture(AppFixture fixture) {
-	String[] clients = { "Microsoft 365 (Partial Graph API)",
-				"Microsoft 365 (OData Service for namespace microsoft.graph)" };
-	System.setProperty("test.azure.app.id", GRAPH_CLIENT_ID.toString());
-	
-	for (String CLIENT : clients) {
-		fixture.config("RestClients.'" + CLIENT + "'.Url", GraphServiceMock.URI);
-
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.baseUri", GraphAuthMock.URI);
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.tokenUri", GraphAuthMock.URI + "/token");
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.authorizeUri", GraphAuthMock.URI + "/authorize");
-
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.appId", GRAPH_CLIENT_ID.toString());
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.secretKey", "1");
-
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.scope", "user.read calendars.read");
-	}
+	System.setProperty("test.azure.app.id", GRAPH_CLIENT_ID);
+	System.setProperty("test.azure.app.secretKey", "1");
+	System.setProperty("test.azure.app.tenantId", "mock");
+    fixture.config("RestClients.'Microsoft 365 (Partial Graph API)'.Url", GraphServiceMock.URI);
+    fixture.config("RestClients.'Microsoft 365 (Partial Graph API)'.Properties.AUTH.baseUri", GraphAuthMock.URI);
+    fixture.config("RestClients.'Microsoft 365 (Partial Graph API)'.Properties.AUTH.secretKey", "1");
+    fixture.config("RestClients.'Microsoft 365 (Partial Graph API)'.Properties.scope", "user.read calendars.read");
   }
 
   public static void configureFixture(WebAppFixture fixture) {
-	String[] clients = { "Microsoft 365 (Partial Graph API)",
-			"Microsoft 365 (OData Service for namespace microsoft.graph)" };
-	System.setProperty("test.azure.app.id", GRAPH_CLIENT_ID.toString());
-
-	for (String CLIENT : clients) {
-		fixture.config("RestClients.'" + CLIENT + "'.Url", GraphServiceMock.URI);
-
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.baseUri", GraphAuthMock.URI);
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.tokenUri", GraphAuthMock.URI + "/token");
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.authorizeUri",
-				GraphAuthMock.URI + "/authorize");
-
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.appId", GRAPH_CLIENT_ID.toString());
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.AUTH.secretKey", "1");
-
-		fixture.config("RestClients.'" + CLIENT + "'.Properties.scope", "user.read calendars.read");
-	}
+	System.setProperty("test.azure.app.id", GRAPH_CLIENT_ID);
+	System.setProperty("test.azure.app.secretKey", "1");
+	System.setProperty("test.azure.app.tenantId", "mock");
+    fixture.config("RestClients.'Microsoft 365 (Partial Graph API)'.Url", GraphServiceMock.URI);
+    fixture.config("RestClients.'Microsoft 365 (Partial Graph API)'.Properties.AUTH.baseUri", GraphAuthMock.URI);
+    fixture.config("RestClients.'Microsoft 365 (Partial Graph API)'.Properties.AUTH.secretKey", "1");
+    fixture.config("RestClients.'Microsoft 365 (Partial Graph API)'.Properties.scope", "user.read calendars.read");
   }
 }

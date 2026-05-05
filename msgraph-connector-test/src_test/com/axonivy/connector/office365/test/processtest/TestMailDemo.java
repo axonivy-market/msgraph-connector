c6d43412269e5f87e1be19979ac72505e75ac77d
thxhuy <xuanhuy4444@gmail.com>
2026-05-05 17:44:18 +0700

    Fix test

--- msgraph-connector-test/src_test/com/axonivy/connector/office365/test/processtest/TestMailDemo.java
+++ msgraph-connector-test/src_test/com/axonivy/connector/office365/test/processtest/TestMailDemo.java
@@ -28,7 +28,7 @@ class TestMailDemo {
   @Test
   void writeMail(BpmClient bpmClient, ISession session) {
     mockMailUi(bpmClient);
-
+	assertThat(System.getProperty("test.azure.app.id")).isEqualTo(GraphTestClient.GRAPH_CLIENT_ID.toString());
     ExecutionResult result = bpmClient.start()
             .process("Demo/ms365Mail/writeMail.ivp")
             .as().session(session)

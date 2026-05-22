a08ac1744eee34a70a1beed93c5b4af4262a4b5d
Hoan Nguyen Quoc <nqhoan@axonactive.com>
2026-05-22 09:38:05 +0700

    Reformat code

--- msgraph-connector-test/src_test/com/axonivy/connector/office365/test/GraphTestClient.java
+++ msgraph-connector-test/src_test/com/axonivy/connector/office365/test/GraphTestClient.java
@@ -13,43 +13,39 @@ import ch.ivyteam.ivy.rest.client.security.CsrfHeaderFeature;
 
 public class GraphTestClient {
 
-	private static final String REST_CLIENT_GRAPH_CLIENT = "RestClients.Microsoft365PartialGraphAPI.";
-	public static final String GRAPH_CLIENT_ID = "Microsoft365PartialGraphAPI";
-	public static final Map<String, String> REST_PROPERTIES = Map.of(
-			Config.URL.property, GraphServiceMock.URI,
-			Config.BASE_URI.property, GraphAuthMock.URI,
-			Config.APP_ID.property, "notmyid",
-			Config.SECRET_KEY.property, "1",
-			Config.SCOPE.property, "user.read calendars.read");
+  private static final String REST_CLIENT_GRAPH_CLIENT = "RestClients.Microsoft365PartialGraphAPI.";
+  public static final String GRAPH_CLIENT_ID = "Microsoft365PartialGraphAPI";
+  public static final Map<String, String> REST_PROPERTIES = Map.of(Config.URL.property, GraphServiceMock.URI,
+      Config.BASE_URI.property, GraphAuthMock.URI,
+      Config.APP_ID.property, "notmyid",
+      Config.SECRET_KEY.property, "1",
+      Config.SCOPE.property, "user.read calendars.read");
 
-	public static void configureFixture(AppFixture fixture) {
-		REST_PROPERTIES.entrySet().forEach(entry -> fixture.config(entry.getKey(), entry.getValue()));
-		fixture.config(Config.FEATURES.property,
-				List.of(CsrfHeaderFeature.class.getName(), JsonFeature.class.getName()));
-	}
+  public static void configureFixture(AppFixture fixture) {
+    REST_PROPERTIES.entrySet().forEach(entry -> fixture.config(entry.getKey(), entry.getValue()));
+    fixture.config(Config.FEATURES.property, List.of(CsrfHeaderFeature.class.getName(), JsonFeature.class.getName()));
+  }
 
-	public static void configureFixture(WebAppFixture fixture) {
-		REST_PROPERTIES.entrySet().forEach(entry -> fixture.config(entry.getKey(), entry.getValue()));
-		fixture.config(Config.FEATURES.property,
-				List.of(CsrfHeaderFeature.class.getName(), JsonFeature.class.getName()));
-	}
+  public static void configureFixture(WebAppFixture fixture) {
+    REST_PROPERTIES.entrySet().forEach(entry -> fixture.config(entry.getKey(), entry.getValue()));
+    fixture.config(Config.FEATURES.property, List.of(CsrfHeaderFeature.class.getName(), JsonFeature.class.getName()));
+  }
 
-	public enum Config {
-		FEATURES(REST_CLIENT_GRAPH_CLIENT.concat("Features")),
-		URL(REST_CLIENT_GRAPH_CLIENT.concat("Url")),
-		BASE_URI(REST_CLIENT_GRAPH_CLIENT.concat("Properties.AUTH.baseUri")),
-		APP_ID(REST_CLIENT_GRAPH_CLIENT.concat("Properties.AUTH.appId")),
-		SECRET_KEY(REST_CLIENT_GRAPH_CLIENT.concat("Properties.AUTH.secretKey")),
-		SCOPE(REST_CLIENT_GRAPH_CLIENT.concat("Properties.scope"));
+  public enum Config {
+    FEATURES(REST_CLIENT_GRAPH_CLIENT.concat("Features")), URL(REST_CLIENT_GRAPH_CLIENT.concat("Url")),
+    BASE_URI(REST_CLIENT_GRAPH_CLIENT.concat("Properties.AUTH.baseUri")),
+    APP_ID(REST_CLIENT_GRAPH_CLIENT.concat("Properties.AUTH.appId")),
+    SECRET_KEY(REST_CLIENT_GRAPH_CLIENT.concat("Properties.AUTH.secretKey")),
+    SCOPE(REST_CLIENT_GRAPH_CLIENT.concat("Properties.scope"));
 
-		private String property;
+    private String property;
 
-		Config(String property) {
-			this.property = property;
-		}
+    Config(String property) {
+      this.property = property;
+    }
 
-		public String getProperty() {
-			return property;
-		}
-	}
+    public String getProperty() {
+      return property;
+    }
+  }
 }
